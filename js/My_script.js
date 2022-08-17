9ae4e1433cec8ac6f812dca239441ce8bb424ff6
Valetina-2608 <valusha2608@gmail.com>
2022-08-17 17:54:45 +0300

    Change function calculation, add function clearInputs

--- js/My_script.js
+++ js/My_script.js
@@ -2,9 +2,9 @@
 
 let my_form = document.getElementById('my_form');
 let input1 = document.getElementById('calorie');
-
-
-let btn_result = document.getElementById('btn_result');
+let input2 = document.getElementById('user_weight');
+let result= document.getElementById('result');
+let btn_clear = document.getElementById('btn_clear');
 
 let all_selects = document.querySelectorAll('select');
 for(let elem of all_selects){
@@ -26,8 +26,13 @@ for(let elem1 of all_radio){
 
 function calculation(){
 if (getValue){
-let input2 = document.getElementById('user_weight');
-let result= document.getElementById('result');
 result.value = Math.round(input1.value/(input2.value*this.value));
 }
+}
+
+btn_clear.addEventListener('click', clearInputs);
+function clearInputs(){
+input1.value = '';
+input2.value = '';
+result.value='';
 }
